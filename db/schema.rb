bca1673efd37e664419e01dd179e54ba6b790fd3
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-06-12 16:00:02 -0600

    Integrated technologies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609213326) do
+ActiveRecord::Schema.define(version: 20170612214052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(version: 20170609213326) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "portfolio_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id", using: :btree
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
@@ -56,4 +64,5 @@ ActiveRecord::Schema.define(version: 20170609213326) do
   end
 
   add_foreign_key "blogs", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,9 @@ end
 		thumb_image: "http://via.placeholder.com/350x200"
 	)
 end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
