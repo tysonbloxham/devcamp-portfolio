f3a56c7130757998b973e0205733cb9a6aa3a1f1
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-20 19:43:54 -0600

    Update portfolio to include attr for position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621060534) do
+ActiveRecord::Schema.define(version: 20170721013028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170621060534) do
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "position"
   end
 
   create_table "technologies", force: :cascade do |t|
