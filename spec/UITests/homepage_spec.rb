fe084bda3f868d62bc3d164faef4923947e4734f
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-11-04 14:58:08 -0600

    Fixed homepage test to close browser and do cleanup

--- spec/UITests/homepage_spec.rb
+++ spec/UITests/homepage_spec.rb
@@ -4,12 +4,13 @@ require_relative '../classes/baseclass.rb'
 require_relative '../classes/homepage.rb'
 
 describe 'BaseClass' do
-  # it 'Goes to the homepage' do
-  # 	browser = HomePage.new()
-  # 	browser.setup("localhost:3000")
-  # 	sleep 5
-  # 	expect(browser.search_bar.displayed?).to eq(true)
-  # end
+  it 'Goes to the homepage' do
+  	browser = HomePage.new()
+  	browser.setup("localhost:3000")
+  	sleep 5
+  	expect(browser.search_bar.displayed?).to eq(true)
+  	browser.close_browser()
+  end
 
   it 'logins in as an admin' do
   	browser = HomePage.new()
