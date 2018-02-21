f761e78e5e0b0783298db6e88ac54601288b8649
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-02-21 15:06:58 -0700

    Added test to create user without email

--- spec/UnitTests/login_unit_spec.rb
+++ spec/UnitTests/login_unit_spec.rb
@@ -16,5 +16,10 @@ RSpec.describe User, type: :model do
       @user.name = nil
       expect(@user).to_not be_valid
     end  
+
+    it 'creates a user with no email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
   end
 end 
