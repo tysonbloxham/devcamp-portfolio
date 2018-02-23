d5efd23375ee5ad2bbde5e9742a8321c90293fdb
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-02-23 09:51:33 -0700

    Added test options and formatting to spec and helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
   config.example_status_persistence_file_path = "examples.txt"
-  config.run_all_when_everything_filtered = true
+  config.run_all_when_everything_filtered = false
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
