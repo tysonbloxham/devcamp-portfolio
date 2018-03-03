6775089811644c7b68d5943482b8ca9263a771ec
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-02 22:53:39 -0700

    Added test to check index can be visited

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe Blog, type: :feature do
+  describe "index" do
+    before(:each) do
+      visit blogs_path
+    end
+
+    it "can be visited" do
+      expect(page.status_code).to eq(200)
+    end
+    
+  end
+end  
