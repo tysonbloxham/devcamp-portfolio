8960dd98ea68fd955e60bb3b5b2c010fea720aa1
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-04 23:58:27 -0700

    Added database cleaner customization to clear out database between tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,7 +52,14 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  # config.before(:suite) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
