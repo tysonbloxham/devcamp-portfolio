89e244543bb22cd6d20b1baaf9d4cbc6e25d7704
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-04 18:32:58 -0700

    Added second blog factory with more consistent information

--- spec/factories/blogs_factory.rb
+++ spec/factories/blogs_factory.rb
@@ -3,4 +3,9 @@ FactoryBot.define do
     title Faker::StarWars.character
     body Faker::StarWars.quote
   end
+
+  factory :blog_two, class: "Blog" do
+    title "This is a title"
+    body "This is a body"
+  end
 end
